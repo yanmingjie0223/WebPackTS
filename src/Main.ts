618eb777ca8f725ddb18061da05b3fe161c4f7b8
yanmingjie0223 <2386068693@qq.com>
2018-06-23 20:49:30 +0800

    [WebpackTS] [ADD] 加入ts装饰器test代码

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,4 @@
+import { Point, Greeter } from "./DecoratorTest";
 
 export default class Main {
 
@@ -8,6 +9,13 @@ export default class Main {
     private initStage() {
         Laya.init(600, 400);
         Laya.stage.scaleMode = Laya.Stage.SCALE_FIXED_AUTO;
+
+        this.greeterTest();
+    }
+
+    private greeterTest(): void {
+        const greeter: Greeter = new Greeter("haha");
+        console.log(greeter.greet("dar", 22), greeter.point = new Point(2, 3));
     }
 
 }
